db9366455514889792f22c164c527fe1f92527e7
jihan2032 <jihan2032@gmail.com>
2016-01-08 01:56:07 +0200

    appraisal modules crud

--- app/controllers/appraisal_system/appraisal_modules_controller.rb
+++ app/controllers/appraisal_system/appraisal_modules_controller.rb
@@ -3,6 +3,8 @@ require_dependency "appraisal_system/application_controller"
 module AppraisalSystem
   class AppraisalModulesController < ApplicationController
 
+    before_action :set_module, only: [:show, :edit, :update, :destroy]
+
     def index
       @modules = AppraisalModule.all
     end
@@ -14,13 +16,39 @@ module AppraisalSystem
     def create
       @appraisal_module = AppraisalModule.new appraisal_module_params
       if @appraisal_module.save
-        redirect_to appraisal_modules_path, notice: 'Appraisal Module created successfully.'
+        redirect_to appraisal_modules_path, notice: 'Module created successfully.'
       else
         render :new
       end
     end
 
+    def edit
+    end
+
+    def update
+      if @appraisal_module.update appraisal_module_params
+        redirect_to appraisal_modules_path, notice: 'Module updated successfully.'
+      else
+        render :edit
+      end
+    end
+
+    def show
+      @competences = @appraisal_module.competences
+    end
+
+    def destroy
+      if @appraisal_module.destroy
+        redirect_to appraisal_modules_path, notice: "Module was successfully removed."
+      end
+    end
+
   private
+
+    def set_module
+      @appraisal_module = AppraisalModule.find params[:id]
+    end
+
     def appraisal_module_params
       params.require(:appraisal_module).permit(
         :name,
